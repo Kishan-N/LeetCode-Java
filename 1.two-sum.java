0e272f59a888a92ed31995b336c6748d1bc02e52
Kishan N <nunnakishan@gmail.com>
2025-03-02 15:12:49 +0530

    Two Sum

---
+++ 1.two-sum.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+            HashMap<Integer,Integer> map = new HashMap<>();
+            for(int i=0;i<nums.length;i++){
+                if(map.containsKey(nums[i])){
+                    return new int[]{i,map.get(nums[i])};
+                }
+                map.put(target-nums[i], i);
+            } 
+            return nums;
+    }
+}
+// @lc code=end
+
